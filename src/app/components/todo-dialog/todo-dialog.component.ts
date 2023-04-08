3e3b89cd1c8d2ea3ff49a80747142cf8e0edd657
Ruben Guevara <rguevarac@hotmail.es>
2023-04-07 20:08:21 -0500

    mosales

--- src/app/components/todo-dialog/todo-dialog.component.ts
+++ src/app/components/todo-dialog/todo-dialog.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
+import { faBars, faCheckSquare, faCheckToSlot, faClock, faClose, faTag, faUser } from '@fortawesome/free-solid-svg-icons';
+import { ToDo } from 'src/app/models/todo.model';
 
+interface InputData {
+  todo: ToDo
+}
+interface OutputData {
+  rpta: boolean
+}
 @Component({
   selector: 'app-todo-dialog',
   templateUrl: './todo-dialog.component.html'
 })
 export class TodoDialogComponent {
 
+  faClose = faClose;
+  faCheckToSlot = faCheckToSlot;
+  faBars = faBars;
+  faUser = faUser;
+  faTag = faTag;
+  faCheckSquare = faCheckSquare;
+  faClock = faClock;
+
+  todo: ToDo;
+  constructor(
+    private dialogRef: DialogRef<OutputData>,
+    @Inject(DIALOG_DATA) private data: InputData,
+  ) {
+    this.todo = data.todo;
+  }
+
+  close() {
+    this.dialogRef.close({
+      rpta: true,
+    });
+  }
 }
--- src/app/pages/board/board.component.ts
+++ src/app/pages/board/board.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Dialog } from '@angular/cdk/dialog';
 
 import { Column, ToDo } from '../../models/todo.model';
+import { TodoDialogComponent } from 'src/app/components/todo-dialog/todo-dialog.component';
 
 @Component({
   selector: 'app-board',
@@ -56,6 +58,9 @@ export class BoardComponent {
     }
   ]
 
+  constructor(
+    private dialog: Dialog
+  ) {}
   drop(e: CdkDragDrop<ToDo[]>) {
     if (e.previousContainer === e.container) {
       moveItemInArray(e.container.data, e.previousIndex, e.currentIndex);
@@ -73,6 +78,20 @@ export class BoardComponent {
     this.columns.push({
       title: 'New Column',
       todos: []
+    })
+  }
+
+  openDialog(todo: ToDo) {
+    const dialogRef =  this.dialog.open(TodoDialogComponent, {
+      minWidth: '300px',
+      maxWidth: '50%',
+      autoFocus: false,
+      data: {
+        todo: todo
+      }
+    });
+    dialogRef.closed.subscribe(output => {
+      console.log(output);
     })
   }
 }
